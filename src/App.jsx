fed3937204a47db1c5c0bed593fbdbd7f706f2d6
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-04-07 11:03:54 -0300

    image submit working

--- src/App.jsx
+++ src/App.jsx
@@ -71,61 +71,22 @@ class App extends Component {
     });
   };
 
-  calculateFaceLocation = (data) => {
-    const clarifaiFace =
-      data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById("inputimage");
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - clarifaiFace.right_col * width,
-      bottomRow: height - clarifaiFace.bottom_row * height,
-    };
-  };
-
-  displayFaceBox = (box) => {
-    this.setState({ box: box });
-  };
-
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
+    console.log(event.target.value);
   };
 
-  onButtonSubmit = () => {
+  onSubmit = () => {
+    console.log('click')
     this.setState({ imageUrl: this.state.input });
-    app.models
-      .predict("face-detection", this.state.input)
-      .then((response) => {
-        console.log("hi", response);
-        if (response) {
-          fetch("http://localhost:3000/image", {
-            method: "put",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              id: this.state.user.id,
-            }),
-          })
-            .then((response) => response.json())
-            .then((count) => {
-              this.setState(Object.assign(this.state.user, { entries: count }));
-            });
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response));
-      })
-      .catch((err) => console.log(err));
+    fetch(
+      `https://api.clarifai.com/v2/models/${MODEL_ID}/outputs`,
+      setupClarifai(imageUrl)
+    )
+      .then((response) => response.json())
+      .then((result) => calculateFaceLocation(result))
+      .catch((error) => console.log("error", error));
   };
-
-  onRouteChange = (route) => {
-    if (route === "signout") {
-      this.setState({ isSignedIn: false });
-    } else if (route === "home") {
-      this.setState({ isSignedIn: true });
-    }
-    this.setState({ route: route });
-  };
-
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
@@ -139,7 +100,7 @@ class App extends Component {
           <Rank name={this.state.user.name} entries={this.state.user.entries} />
           <ImageLinkForm
             onInputChange={this.onInputChange}
-            onButtonSubmit={this.onButtonSubmit}
+            onSubmit={this.onSubmit}
           />
           <FaceRecongnition box={box} imageUrl={imageUrl} />
         </div>
--- src/components/FaceRecongnition/FaceRecongnition.jsx
+++ src/components/FaceRecongnition/FaceRecongnition.jsx
@@ -1,9 +1,8 @@
-
-const FaceRecongnition = ( imageUrl ) => {
-  console.log(imageUrl);
+const FaceRecongnition = ({ imageUrl }) => {
+  // console.log(imageUrl);
   return (
     <div className="flex justify-center">
-      <img height={700} width={700} src={imageUrl} />
+      <img src={imageUrl} height={700} width={700} />
     </div>
   );
 };
