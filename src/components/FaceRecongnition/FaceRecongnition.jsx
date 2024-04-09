dc8fd6cc84ee8ce7272426027d299a66ec7958d0
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-04-09 20:16:09 -0300

    face recognition working

--- src/components/FaceRecongnition/FaceRecongnition.jsx
+++ src/components/FaceRecongnition/FaceRecongnition.jsx
@@ -2,16 +2,18 @@ import "./FaceRecongnition.css";
 const FaceRecongnition = ({ imageUrl, box }) => {
   return (
     <div className="flex justify-center">
-      <img className="inputimage" src={imageUrl} height="500" width="500px" />
-      <div
-        className="bounding-box"
-        style={{
-          top: box.topRow,
-          right: box.rightCol,
-          bottom: box.bottomRow,
-          left: box.leftCol,
-        }}
-      ></div>
+      <div className="absolute mt-4">
+        <img className="inputimage" src={imageUrl} height="500" width="auto" />
+        <div
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol,
+          }}
+        ></div>
+      </div>
     </div>
   );
 };
