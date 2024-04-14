40b11e0118df1e0edf1cfcd314efcdbe45e24c69
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-04-14 20:32:26 -0300

    added database

--- public/server.js
+++ public/server.js
@@ -1,6 +1,23 @@
-import express from "express";
+import express, { response } from "express";
 import bcrypt from "bcrypt";
 import cors from "cors";
+import knex from "knex";
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "41277714",
+    database: "smart-brain",
+  },
+});
+
+db.select("*")
+  .from("users")
+  .then((data) => {
+    console.log(data);
+  });
 
 const app = express();
 
@@ -27,7 +44,6 @@ const database = {
       joined: new Date(),
     },
   ],
-  login: [{}],
 };
 
 app.get("/", (req, res) => {
@@ -51,43 +67,44 @@ app.post("/register", (req, res) => {
   bcrypt.hash(password, saltRounds, function (err, hash) {
     console.log(hash);
   });
-  database.users.push({
-    id: "125",
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date(),
-  });
-  res.json(database.users[database.users.length - 1]);
+
+  db("users")
+    .returning("*")
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date(),
+    })
+    .then((user) => {
+      res.json(user[0]);
+    })
+    .catch((err) => res.status(400).json("unable to register"));
 });
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(400).json("not found");
-  }
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("Not Found");
+      }
+    })
+    .catch((err) => res.status(400).json("Error getting user"));
 });
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(400).json("not found");
-  }
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      console.log(entries);
+    });
 });
 app.listen(3000, () => {
   console.log("app is running on port 3000");
