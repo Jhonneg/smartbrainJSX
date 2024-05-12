912aeafc9504b06c5b5aed3ce8721492eb2d8037
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-05-12 06:36:08 -0300

    created modal.css

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,21 @@
+import { Component } from "react";
+import ReactDOM from "react-dom/client";
 
+const modalRoot = document.getElementById("modal-root");
 
-export default function Modal() {
-  return (
-    <div>Modal</div>
-  )
+export default class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.el = document.createElement("div");
+  }
+  componentDidMount() {
+    modalRoot.appendChild(this.el);
+  }
+  componentWillUnmount() {
+    modalRoot.appendChild(this.el);
+  }
+
+  render() {
+    return ReactDOM.createPortal(this.props.children, this.el);
+  }
 }
-
