e916b12b9f5ce83f626d4749638972b9f82754ef
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-05-19 18:18:51 -0300

    added favcon

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -15,7 +15,7 @@ const defaultOptions = {
 
 function Logo() {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center flex-col">
       <Tilt
         className="Tilt"
         options={{ max: 55 }}
@@ -25,6 +25,16 @@ function Logo() {
           <img className="p-6" alt="logo" src={brain} />
         </div>
       </Tilt>
+      <a href="https://github.com/Jhonneg">
+        <p className="inline-block mx-1">Build by </p>
+        <img
+          className="inline-block"
+          width="32"
+          height="32"
+          src="https://img.icons8.com/glyph-neue/64/FFFFFF/github.png"
+          alt="github"
+        />
+      </a>
     </div>
   );
 }
