cda802fa3bb5c223a35e8e02810a19fed08255e4
Jhonne <jonee4339@hotmail.com>
2025-11-03 05:02:10 -0300

    fixed box bug

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 export const metadata = {
   title: "Face Recognition App",
   description: "A Next.js application for face recognition using Clarifai API",
+  icons: "https://img.icons8.com/ios-filled/50/FFFFFF/source-code.png",
 };
 
 export default function RootLayout({ children }) {
